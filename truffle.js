8fd6456de3473ef0d024703f49dd7a933a0771f8
Rahul Sethuram <rahul@rahuls-mbp.lan>
2018-03-14 09:24:09 -0700

    Set ganache configuration

--- truffle.js
+++ truffle.js
@@ -1,4 +1,7 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-};
+  ganache: {
+    host: '127.0.0.1',
+    port: 7545,
+    network_id: '*' // match any network
+  }
+}
